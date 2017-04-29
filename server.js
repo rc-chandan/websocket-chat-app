cb15c78df233b0b644ad2ec8434a81d9b3259017
Chandan Kumar <chandan316@outlook.com>
2017-04-29 11:11:58 +0530

    configured for heroku deployment

--- server.js
+++ server.js
@@ -42,6 +42,8 @@ io.on('connection', function(socket){
 
 });
 
-http.listen(3000, function(){
-  	console.log('listening on *:3000');
+const port = Number(process.env.PORT || 3000);
+
+http.listen(port, function(){
+  	console.log('server listening on ' + port);
 });
